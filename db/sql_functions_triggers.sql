57116f1633e85d12b28a766d97f62efa6f296efa
KatyOrozco <katestefy.oro96@gmail.com>
2018-07-21 17:02:15 -0500

    corrección de función triggers

--- db/sql_functions_triggers.sql
+++ db/sql_functions_triggers.sql
@@ -1,13 +1,12 @@
 -- Crear funcion de trigger
-create or replace function tg_kilomentraje_semanal() returns trigger AS
+create or replace function tg_kilometraje_semanal() returns trigger AS
 $$
-  DECLARE i auto%rowtype;
   DECLARE delta_horas numeric;
   DECLARE delta_km numeric;
   DECLARE km_semanal numeric;
 begin
-  select time NEW.updated_at - time OLD.updated_at into delta_horas;
-  select time NEW.current_km - time OLD.current_km into delta_km;
+  delta_horas NEW.updated_at - time OLD.updated_at;
+  delta_km NEW.current_km - time OLD.current_km;
   if (delta_horas > 0) && (NEW.current_km != OLD.current_km)
     km_semanal = (delta_km/delta_horas)*168;
     update cars set week_km = km_semanal where id = OLD.id;
