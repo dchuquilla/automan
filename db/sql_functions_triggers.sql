cc2c811ed126c06d2b93c0f42f8f8f383df991bf
dchuquilla <dario.chuquilla@gmail.com>
2018-07-21 16:52:35 -0500

    Triggers para auto: control de kilometraje

---
+++ db/sql_functions_triggers.sql
@@ -0,0 +1,24 @@
+-- Crear funcion de trigger
+create or replace function tg_kilomentraje_semanal() returns trigger AS
+$$
+  DECLARE i auto%rowtype;
+  DECLARE delta_horas numeric;
+  DECLARE delta_km numeric;
+  DECLARE km_semanal numeric;
+begin
+  select time NEW.updated_at - time OLD.updated_at into delta_horas;
+  select time NEW.current_km - time OLD.current_km into delta_km;
+  if (delta_horas > 0) && (NEW.current_km != OLD.current_km)
+    km_semanal = (delta_km/delta_horas)*168;
+    update cars set week_km = km_semanal where id = OLD.id;
+  end if;
+  return NEW;
+end
+$$ language 'plpgsql';
+
+
+-- Crear triggers con metodo creado
+create trigger procesarCambiosKm
+  after update on cars
+    for each row
+      execute procedure tg_kilomentraje_semanal();
