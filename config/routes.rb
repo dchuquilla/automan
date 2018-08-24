efc1c5ce4dbbe2f0fc315b3aeb52a7bca35ab106
dchuquilla <dario.chuquilla@gmail.com>
2018-08-24 16:58:41 -0500

    Minimo viable terminado

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
       get 'select'
     end
   end
-  resources :owners, except: [:index]
+  resources :owners
   resources :settings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
