9c17870515f9900e1121704d07de0d2479f2f39e
dchuquilla <dario.chuquilla@gmail.com>
2018-08-25 02:46:49 -0500

    uso de variables ENV para datos de sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => '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',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'automan-ec.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
