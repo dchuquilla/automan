3db687798d6a312e58256deeaeae18b47545d75e
Dario Chuquilla <dario.chuquilla@gmail.com>
2018-09-18 16:24:08 -0500

    Proceso de reseteo completado

--- db/migrate/20180912210353_run_sql_scripts.rb
+++ db/migrate/20180912210353_run_sql_scripts.rb
@@ -1,97 +1,95 @@
 class RunSqlScripts < ActiveRecord::Migration[5.2]
-  def up
-    raw_sql = " -- Crear funcion de trigger "\
-              " CREATE OR REPLACE FUNCTION tg_kilometraje_semanal() "\
-              "  RETURNS trigger "\
-              "  LANGUAGE plpgsql "\
-              " AS $function$ "\
-              "   DECLARE delta_horas numeric; "\
-              "   DECLARE delta_km numeric; "\
-              "   DECLARE km_semanal numeric; "\
-              "   DECLARE nuevo_update Timestamp Without Time Zone; "\
-              "   DECLARE antiguo_update Timestamp Without Time Zone; "\
-              "   DECLARE nuevo_km_actual numeric; "\
-              "   DECLARE antiguo_km_actual numeric; "\
-              " BEGIN "\
-              "   nuevo_update = NEW.updated_at; "\
-              "   antiguo_update = OLD.updated_at; "\
-              "   nuevo_km_actual = NEW.current_km; "\
-              "   antiguo_km_actual = OLD.current_km; "\
-              "   SELECT EXTRACT(EPOCH FROM nuevo_update-antiguo_update)/3600 INTO delta_horas; "\
-              "   delta_km = nuevo_km_actual - antiguo_km_actual; "\
-              "   if (delta_horas > 0) AND (NEW.current_km <> OLD.current_km) then "\
-              "     km_semanal = (delta_km/delta_horas)*168; "\
-              "     update cars set week_km = km_semanal where id = OLD.id; "\
-              "   end if; "\
-              "   return NEW; "\
-              " END "\
-              " $function$; "\
-              " -- Crear triggers con metodo creado "\
-              " CREATE TRIGGER procesarCambiosKmActual  "\
-              "   AFTER UPDATE ON cars  "\
-              "     FOR EACH ROW  "\
-              "       EXECUTE PROCEDURE tg_kilometraje_semanal(); "\
-
-              " CREATE OR REPLACE FUNCTION tg_mantenimientos_basicos() "\
-              "  RETURNS trigger "\
-              "  LANGUAGE plpgsql "\
-              " AS $function$ "\
-              "   DECLARE i settings%rowtype; "\
-              "   DECLARE setting_id integer; "\
-              "   DECLARE contador integer; "\
-              "   DECLARE edad integer; "\
-              "   DECLARE calculated_km integer; "\
-              "   DECLARE km_estimado integer; "\
-              "   DECLARE mes_estimado integer; "\
-              "   DECLARE fecha_estimada Timestamp Without Time Zone; "\
-              "   DECLARE tipo varchar(255); "\
-              " BEGIN "\
-              "   SELECT date_part('year', now()) - NEW.year INTO edad; "\
-              "   FOR i IN SELECT * FROM settings WHERE car_age = edad AND car_type = NEW.car_type LOOP "\
-              "     -- Obtener un numero aleatorio de un rango, meses y Km. "\
-              "     SELECT floor(random() * (i.km_max-i.km_min+1) + i.km_min)::int INTO calculated_km; "\
-              "     SELECT calculated_km + NEW.current_km INTO km_estimado; "\
-              "     SELECT floor(random() * (i.month_max-i.month_min+1) + i.month_min)::int INTO mes_estimado; "\
-              "     -- generar fecha furtura aproximanda de mantenimiento. "\
-              "     SELECT now() + mes_estimado * interval '1 month' into fecha_estimada; "\
-              "     -- Insertar mantenimientos básicos "\
-              "     INSERT INTO user_car_settings (car_id, maintenance_type, km_estimated, month_estimated, created_at, updated_at) VALUES (NEW.id, i.maintenance_type, calculated_km, mes_estimado, now(), now()); "\
-              "     SELECT id FROM user_car_settings WHERE car_id = NEW.id AND maintenance_type = i.maintenance_type AND km_estimated = calculated_km AND month_estimated = mes_estimado INTO setting_id; "\
-              "     INSERT INTO maintenance_histories (car_id, maintenance_type, estimated_km, notified, scheduled_date, status, user_car_setting_id, created_at, updated_at) VALUES (NEW.id, i.maintenance_type, km_estimado, FALSE, fecha_estimada, 'Pendiente', setting_id, now(), now()); "\
-              "   END LOOP; "\
-              "   return NEW; "\
-              " END "\
-              " $function$; "\
-              " CREATE TRIGGER crearMantenimientoBasicos AFTER INSERT ON cars FOR EACH ROW EXECUTE PROCEDURE tg_mantenimientos_basicos(); "\
-
-              " -- Gestion de manteminientos recurrentes "\
-              " CREATE OR REPLACE FUNCTION tg_mantenimientos_recurrentes() "\
-              "  RETURNS trigger "\
-              "  LANGUAGE plpgsql "\
-              " AS $function$ "\
-              "   DECLARE i user_car_settings%rowtype; "\
-              "   DECLARE contador integer; "\
-              "   DECLARE edad integer; "\
-              "   DECLARE km_estimado integer; "\
-              "   DECLARE mes_estimado integer; "\
-              "   DECLARE fecha_estimada Timestamp Without Time Zone; "\
-              "   DECLARE tipo varchar(255); "\
-              " BEGIN "\
-              "   if NEW.status = 'Completado' AND NEW.status <> OLD.status then "\
-              "     SELECT * FROM user_car_settings where id = NEW.user_car_setting_id INTO i; "\
-              "     SELECT NEW.review_km + i.km_estimated INTO km_estimado; "\
-              "     SELECT NEW.review_date + i.month_estimated * interval '1 month' into fecha_estimada; "\
-              "     INSERT INTO maintenance_histories ( car_id, maintenance_type, estimated_km, notified, scheduled_date, status, created_at, updated_at) VALUES ( NEW.car_id, NEW.maintenance_type, km_estimado, FALSE, fecha_estimada, 'Pendiente', now(), now()); "\
-              "   end if; "\
-              "   return NEW; "\
-              " END "\
-              " $function$; "\
-              " CREATE TRIGGER crearMantenimientoRecurrentes AFTER UPDATE ON maintenance_histories FOR EACH ROW EXECUTE PROCEDURE tg_mantenimientos_recurrentes(); "\
-
-    execute raw_sql
+  def change
+    raw_sql_1 = " CREATE OR REPLACE FUNCTION tg_kilometraje_semanal() \n"\
+              "  RETURNS trigger \n"\
+              "  LANGUAGE plpgsql \n"\
+              " AS $function$ \n"\
+              "   DECLARE delta_horas numeric; \n"\
+              "   DECLARE delta_km numeric; \n"\
+              "   DECLARE km_semanal numeric; \n"\
+              "   DECLARE nuevo_update Timestamp Without Time Zone; \n"\
+              "   DECLARE antiguo_update Timestamp Without Time Zone; \n"\
+              "   DECLARE nuevo_km_actual numeric; \n"\
+              "   DECLARE antiguo_km_actual numeric; \n"\
+              " BEGIN \n"\
+              "   nuevo_update = NEW.updated_at; \n"\
+              "   antiguo_update = OLD.updated_at; \n"\
+              "   nuevo_km_actual = NEW.current_km; \n"\
+              "   antiguo_km_actual = OLD.current_km; \n"\
+              "   SELECT EXTRACT(EPOCH FROM nuevo_update-antiguo_update)/3600 INTO delta_horas; \n"\
+              "   delta_km = nuevo_km_actual - antiguo_km_actual; \n"\
+              "   if (delta_horas > 0) AND (NEW.current_km <> OLD.current_km) then \n"\
+              "     km_semanal = (delta_km/delta_horas)*168; \n"\
+              "     update cars set week_km = km_semanal where id = OLD.id; \n"\
+              "   end if; \n"\
+              "   return NEW; \n"\
+              " END \n"\
+              " $function$; "
+    execute raw_sql_1
+    
+    raw_sql_2 = " CREATE TRIGGER procesarCambiosKmActual  \n"\
+              "   AFTER UPDATE ON cars  \n"\
+              "     FOR EACH ROW  \n"\
+              "       EXECUTE PROCEDURE tg_kilometraje_semanal();"
+    execute raw_sql_2
+    
+    raw_sql_3 = " CREATE OR REPLACE FUNCTION tg_mantenimientos_basicos() \n"\
+              "  RETURNS trigger \n"\
+              "  LANGUAGE plpgsql \n"\
+              " AS $function$ \n"\
+              "   DECLARE i settings%rowtype; \n"\
+              "   DECLARE setting_id integer; \n"\
+              "   DECLARE contador integer; \n"\
+              "   DECLARE edad integer; \n"\
+              "   DECLARE calculated_km integer; \n"\
+              "   DECLARE km_estimado integer; \n"\
+              "   DECLARE mes_estimado integer; \n"\
+              "   DECLARE fecha_estimada Timestamp Without Time Zone; \n"\
+              "   DECLARE tipo varchar(255); \n"\
+              " BEGIN \n"\
+              "   SELECT date_part('year', now()) - NEW.year INTO edad; \n"\
+              "   FOR i IN SELECT * FROM settings WHERE car_age = edad AND car_type = NEW.car_type LOOP \n"\
+              "     SELECT floor(random() * (i.km_max-i.km_min+1) + i.km_min)::int INTO calculated_km; \n"\
+              "     SELECT calculated_km + NEW.current_km INTO km_estimado; \n"\
+              "     SELECT floor(random() * (i.month_max-i.month_min+1) + i.month_min)::int INTO mes_estimado; \n"\
+              "     SELECT now() + mes_estimado * interval '1 month' into fecha_estimada; \n"\
+              "     INSERT INTO user_car_settings (car_id, maintenance_type, km_estimated, month_estimated, created_at, updated_at) VALUES (NEW.id, i.maintenance_type, calculated_km, mes_estimado, now(), now()); \n"\
+              "     SELECT id FROM user_car_settings WHERE car_id = NEW.id AND maintenance_type = i.maintenance_type AND km_estimated = calculated_km AND month_estimated = mes_estimado INTO setting_id; \n"\
+              "     INSERT INTO maintenance_histories (car_id, maintenance_type, estimated_km, notified, scheduled_date, status, user_car_setting_id, created_at, updated_at) VALUES (NEW.id, i.maintenance_type, km_estimado, FALSE, fecha_estimada, 'Pendiente', setting_id, now(), now()); \n"\
+              "   END LOOP; \n"\
+              "   return NEW; \n"\
+              " END \n"\
+              " $function$; "
+    execute raw_sql_3
+    
+    raw_sql_4 = " CREATE TRIGGER crearMantenimientoBasicos AFTER INSERT ON cars FOR EACH ROW EXECUTE PROCEDURE tg_mantenimientos_basicos(); "
+    execute raw_sql_4
+    
+    raw_sql_5 = " CREATE OR REPLACE FUNCTION tg_mantenimientos_recurrentes() \n"\
+              "  RETURNS trigger \n"\
+              "  LANGUAGE plpgsql \n"\
+              " AS $function$ \n"\
+              "   DECLARE i user_car_settings%rowtype; \n"\
+              "   DECLARE contador integer; \n"\
+              "   DECLARE edad integer; \n"\
+              "   DECLARE km_estimado integer; \n"\
+              "   DECLARE mes_estimado integer; \n"\
+              "   DECLARE fecha_estimada Timestamp Without Time Zone; \n"\
+              "   DECLARE tipo varchar(255); \n"\
+              " BEGIN \n"\
+              "   if NEW.status = 'Completado' AND NEW.status <> OLD.status then \n"\
+              "     SELECT * FROM user_car_settings where id = NEW.user_car_setting_id INTO i; \n"\
+              "     SELECT NEW.review_km + i.km_estimated INTO km_estimado; \n"\
+              "     SELECT NEW.review_date + i.month_estimated * interval '1 month' into fecha_estimada; \n"\
+              "     INSERT INTO maintenance_histories ( car_id, maintenance_type, estimated_km, notified, scheduled_date, status, created_at, updated_at) VALUES ( NEW.car_id, NEW.maintenance_type, km_estimado, FALSE, fecha_estimada, 'Pendiente', now(), now()); \n"\
+              "   end if; \n"\
+              "   return NEW; \n"\
+              " END \n"\
+              " $function$; "
+    execute raw_sql_5
+    
+    raw_sql_6 = " CREATE TRIGGER crearMantenimientoRecurrentes AFTER UPDATE ON maintenance_histories FOR EACH ROW EXECUTE PROCEDURE tg_mantenimientos_recurrentes(); "
+    execute raw_sql_6
   end
 
-  def down
-    raise ActiveRecord::IrreversibleMigration
-  end
 end
